f4b36be1cd846dc96255ab40f79c101d27d856b7
IliaLataria01 <ilataria@solvd.com>
2024-02-01 23:41:16 +0400

    Added functionality to enter Jenkins,with login

--- jenkins/run_jenkins.sh
+++ jenkins/run_jenkins.sh
@@ -12,11 +12,14 @@ HOST_PORT=8080
 # Build the Docker image
 docker build -t $IMAGE_NAME .
 
-# Stop the container if it's already running and remove it
-docker stop $CONTAINER_NAME || true
-docker rm $CONTAINER_NAME || true
-
 # Run the Jenkins container
 docker run -d --name $CONTAINER_NAME -p $HOST_PORT:8080 -p 50000:50000 $IMAGE_NAME
 
 echo "Jenkins should be available at http://localhost:$HOST_PORT"
+
+echo "Waiting Jenkins to start"
+sleep 15
+
+
+
+
---
+++ jenkins/security.groovy
@@ -0,0 +1,16 @@
+#!groovy
+
+import jenkins.model.*
+import hudson.security.HudsonPrivateSecurityRealm
+import hudson.security.FullControlOnceLoggedInAuthorizationStrategy
+
+def instance = Jenkins.getInstance()
+
+def hudsonRealm = new HudsonPrivateSecurityRealm(false)
+hudsonRealm.createAccount("admin", "admin")
+instance.setSecurityRealm(hudsonRealm)
+
+def strategy = new FullControlOnceLoggedInAuthorizationStrategy()
+strategy.setAllowAnonymousRead(false)
+instance.setAuthorizationStrategy(strategy)
+instance.save()
