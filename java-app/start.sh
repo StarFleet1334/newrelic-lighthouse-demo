b3e63aa254d014b8e423a4026e9c2881edb87876
IliaLataria01 <latariailia6@gmail.com>
2024-02-06 17:42:22 +0400

    x:

--- java-app/start.sh
+++ java-app/start.sh
@@ -3,22 +3,38 @@
 
 # Set up Lighthouse
 npm install -g lighthouse
+
 # if permission denied, use sudo
 
 # Source the .env file to load the environment variables
 source .env
 
 # Run Lighthouse audit and save the report in JSON format at the root of the java-app folder
-/usr/local/bin/lighthouse $LIGHTHOUSE_URL --output=json --output-path=./lighthouse_report.json
+#/usr/local/bin/lighthouse $LIGHTHOUSE_URL --output=json --output-path=./lighthouse_report.json
+
+echo "Running Lighthouse..."
+npx lighthouse $LIGHTHOUSE_URL --output=json --output-path=./lighthouse_report.json
+
+
+# Check if Maven is installed, install if not
+if ! command -v mvn > /dev/null; then
+    echo "Maven is not installed. Installing Maven..."
+    # Add your Maven installation command here. For example, for Ubuntu it might be:
+    # sudo apt-get install -y maven
+fi
+
 
 ## Move the Lighthouse report to the target directory
-
+echo "Packaging the application..."
 mvn package
 
+echo "Moving the Lighthouse report..."
 mv ./lighthouse_report.json ./target/lighthouse_report.json
 
 # Go to the target directory
 cd ./target
 
 # To run the app with instrumentation:
+echo "Running the application with New Relic instrumentation..."
+
 java -javaagent:./newrelic/newrelic.jar -jar newrelic-lighthouse-1.0-SNAPSHOT.jar
